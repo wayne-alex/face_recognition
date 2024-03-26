8e0a41399ad6385ff325c98d5d4a96f269f93316
Wayne Alex <waynealex9914@gmail.com>
2024-03-26 12:07:52 +0300

    Ensure pip is there

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,8 @@
-pip install -r requirements.txt
+# Install pip
+python3.9 -m ensurepip
+
+# Install Python dependencies
+python3.9 -m pip install -r requirements.txt
+
+# Collect static files
 python3.9 manage.py collectstatic --noinput
